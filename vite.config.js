a4c4ab90364c3cee6f369cb2303af99fe22d7ea2
Félix García Sánchez <eudaimov@gmail.com>
2025-03-24 12:04:30 +0100

    Configurado en base

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     host: true, // Esto expone el servidor en tu red local
     port: 5173, // Cambia el puerto si lo necesitas
   },
+  base: '/grupoSADE3d/',
   build:{
     outDir:'docs',
     emptyOutDir: true
